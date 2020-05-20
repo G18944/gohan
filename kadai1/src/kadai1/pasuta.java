2354d8800056dace4f6e01fa83ba8c4d95f5faef
onomoegi <onomoegi@192.168.10.112>
2020-05-20 15:46:01 +0900

    pompom

---
+++ kadai1/src/kadai1/pasuta.java
@@ -0,0 +1,11 @@
+package kadai1;
+
+public class pasuta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("私の好きな食べ物はパスタです。種類がたくさんあって自分で作るのも簡単"
+		+ "なのでよく食べます");
+	}
+
+}
